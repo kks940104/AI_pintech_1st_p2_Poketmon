faac6da70fa53f0450d26f7b439e23e2481a80d4
kks940104 <kks940104@gmail.com>
2024-11-13 12:14:51 +0900

    Add files via upload

---
+++ src/main/java/org/koreait/main/controllers/MainController.java
@@ -0,0 +1,23 @@
+package org.koreait.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("addCss",
+                List.of("member/test1", "member/test2"));
+        model.addAttribute("addScript",
+                new String[] {"member/test1", "member/test2"});
+        model.addAttribute("pageTitle","MainForm");
+        return "front/main/index";
+    }
+}
